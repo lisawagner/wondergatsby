2c9fbe57be0bd162504a275afb0fdb925fe7294c
Lisa Wagner <lisawagner.online@gmail.com>
2021-04-11 13:15:05 -0700

    tutorial changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,43 @@
-import React from "react"
-import Header from "../components/header"
+import React from "react";
+import Header from "../components/header";
+import Container from "../components/container";
+import * as styles from "./about.module.css";
+import { Link } from "gatsby";
+
+const User = (props) => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+);
 
 export default function About() {
+  console.log(styles);
   return (
     <div style={{ color: `teal` }}>
-        <Header headerText="-about page header-"/>
-        <Header headerText="another header"/>
-    
+      <Link to="/">Home</Link>
+      <Link to="/about/">About</Link>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="-about page header-" />
+      <Header headerText="another header" />
+      <Container>
+        <h1>This is a CSS Module called Container</h1>
+        <p>It was added to the About page as a Component</p>
+        <User
+          username="Maria Randall"
+          avatar="https://raw.githubusercontent.com/gatsbyjs/gatsby/master/docs/docs/tutorial/part-two/pexels-daniel-xavier-1102341.jpg"
+          excerpt="I'm Maria Randall. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        />
+        <User
+          username="Daniela Dewitt"
+          avatar="https://raw.githubusercontent.com/gatsbyjs/gatsby/master/docs/docs/tutorial/part-two/pexels-guilherme-almeida-1858175.jpg"
+          excerpt="I'm Daniela Dewitt. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        />
+      </Container>
       <p>Such wow. Very React.</p>
     </div>
-  )
-  }
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
-import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
-import Layout from "../components/layout"
+import React from "react";
+import { Link } from "gatsby";
+import Header from "../components/header";
+import Layout from "../components/layout";
 
 export default function Home() {
   return (
     <Layout>
+      <Link to="/">Home</Link>
       <Link to="/about/">About</Link>
       <Link to="/contact/">Contact</Link>
-      <Header headerText="I’m in a layout component!"/>
+      <Header headerText="I’m in a layout component!" />
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </Layout>
   );
