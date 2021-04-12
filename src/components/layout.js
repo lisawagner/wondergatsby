9d18b2b87189334479a4d9602d3fed598089b4ea
Lisa Wagner <lisawagner.online@gmail.com>
2021-04-12 11:46:20 -0700

    basic navigation added

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "gatsby";
 import "./layout.css";
 const ListLink = (props) => (
-  <li>
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
     <Link to={props.to}>{props.children}</Link>
   </li>
 );
@@ -10,16 +10,17 @@ const ListLink = (props) => (
 export default function Layout({ children }) {
   return (
     <div className="container">
-      <header>
-        <Link to="/">
-          <h3>MySweetSite</h3>
+      <header style={{ marginBottom: `1.5rem` }}>
+        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+          <h3 style={{ display: `inline` }}>MySweetSite</h3>
         </Link>
+
+        <ul style={{ listStyle: `none`, float: `right` }}>
+          <ListLink to="/">Home</ListLink>
+          <ListLink to="/about/">About</ListLink>
+          <ListLink to="/contact/">Contact</ListLink>
+        </ul>
       </header>
-      <ul>
-        <ListLink to="/">Home</ListLink>
-        <ListLink to="/about/">About</ListLink>
-        <ListLink to="/contact/">Contact</ListLink>
-      </ul>
       {children}
     </div>
   );
